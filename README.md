03fb47c3d6e713ad9d6cd001989c36d408045f61
Gustavo Jacintho <gustavo.jacintho-consultorexterno@rabobank.com>
2022-10-10 10:27:00 -0300

    Python app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# lab-devops-cloud-grupo20
+## Laboratório DevOps
+
+Aplicação Simples em Python/Flask com teste usando Unittest integrado ao Github Actions fazendo deploy no Heroku para exercitar uma Pipeline DevOps
---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+import os
+
+app = Flask(__name__)
+
+csrf = CSRFProtect(app)
+
+@app.route("/")
+def pagina_inicial():
+    return "Laboratório DevOps - FIAP 8ASO v02"
+
+if __name__ == '__main__':
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)
